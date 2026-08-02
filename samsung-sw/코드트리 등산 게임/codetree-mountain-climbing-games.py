6c7c510f5b55965dd8d0b900eb28304ea24dd391
JeongMinLee <129810866+jeongminllee@users.noreply.github.com>
2026-08-02 19:11:14 +0900

    [Passed] 코드트리 등산 게임 (3935ms, 142MB)

--- samsung-sw/코드트리 등산 게임/codetree-mountain-climbing-games.py
+++ samsung-sw/코드트리 등산 게임/codetree-mountain-climbing-games.py
@@ -1,12 +1,13 @@
-h_max = 10**6
+MAX = 1_000_000
 
-class SegmentTree :
-    def __init__(self, max_size):
+
+class Tree : 
+    def __init__(self, size):
         self.base = 1
-        while self.base < max_size :
+        while self.base < size :
             self.base <<= 1
 
-        self.tree = [(0, 0) for _ in range(self.base << 1)]
+        self.tree = [(0, 0)] * (self.base * 2)
 
     def get(self, left, right) :
         res = (0, 0)
@@ -14,34 +15,37 @@ class SegmentTree :
         right += self.base
 
         while left <= right :
-            if left & 1 :
+            if left & 1 : 
                 res = max(res, self.tree[left])
                 left = (left + 1) >> 1
+
             else :
                 left >>= 1
 
             if right & 1 :
                 right >>= 1
+
             else :
                 res = max(res, self.tree[right])
                 right = (right - 1) >> 1
 
         return res
-    
-    def update(self, idx, value) :
-        ti = idx + self.base
-        self.tree[ti] = (value, idx)
 
-        while (ti := ti >> 1) > 0 :
+    def update(self, idx, val) :
+        ti = idx + self.base
+        self.tree[ti] = (val, idx)
+
+        while ti > 0 :
+            ti >>= 1
             self.tree[ti] = max(self.tree[ti * 2], self.tree[ti * 2 + 1])
 
 def add_func(h) :
-    dp, _ = seg.get(0, h - 1)
+    dp, _ = tree.get(0, h-1)
     dp += 1
     A.append(h)
     dp_idx.append(dp)
     dp_val[h].append(dp)
-    seg.update(h, dp)
+    tree.update(h, dp)
 
 def remove_func() :
     h = A.pop()
@@ -52,33 +56,37 @@ def remove_func() :
     if dp_val[h] :
         dp = dp_val[h][-1]
 
-    seg.update(h, dp)
+    tree.update(h, dp)
 
 def get_score(m_idx) :
-    dp, idx = seg.get(0, h_max)
-    return (dp_idx[m_idx] + dp - 1) * 10 ** 6 + idx
+    dp, idx = tree.get(0, MAX)
+    return (dp_idx[m_idx] + dp - 1) * 10**6 + idx
 
-A = []
-dp_idx = []
-dp_val = [[] for _ in range(h_max + 1)]
-seg = SegmentTree(h_max + 1)
 
-if __name__ == "__main__" :
+if __name__ == '__main__' :
     Q = int(input())
+    A = []
+    dp_idx = []
+    dp_val = [[] for _ in range(MAX + 1)]
+    tree = Tree(MAX + 1)
+
     for _ in range(Q) :
         query = list(map(int, input().split()))
-        cmd, *v = query
+        cmd = query[0]
 
         if cmd == 100 :
-            n, *mountain = v
-            for h in mountain :
+            _, n, *lst = query
+            for h in lst :
                 add_func(h)
 
         elif cmd == 200 :
-            add_func(v[0])
+            _, h = query
+            add_func(h)
 
         elif cmd == 300 :
-            remove_func() 
+            remove_func()
 
         elif cmd == 400 :
-            print(get_score(v[0] - 1))
+            _, m_idx = query
+            res = get_score(m_idx - 1)
+            print(res)
